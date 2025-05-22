b98fbca22fd9bf09fffa774193ae03eb58f4efde
Simmi Amin <simran.amin@kraken.tech>
2025-05-22 16:52:48 +0100

    Add first commit
    Adds the basic structure of the repo including test videos/images

---
+++ parser.py
@@ -0,0 +1,15 @@
+def process_video():
+    # calls ffpmeg and exports into folder of images
+    pass
+
+def create_instance_file():
+    # processes folder of image and creates instance.json
+    pass
+
+def process_image():
+    # parses image into a single instance and appends it onto instance file
+    pass
+
+def start_detection():
+    # calls the ai tool.
+    pass

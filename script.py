b349d4331722f8bc9ebb0daba87491e0da352876
Simmi Amin <simran.amin@kraken.tech>
2025-05-23 17:08:21 +0100

    Pass metadata to Claude
    - Stores the metadata for each video in a .json file

--- script.py
+++ script.py
@@ -4,36 +4,49 @@ import pandas as pd
 import psycopg2
 import base64
 import ast
+import json
+
+import parser
 
 
 LOCAL_DB_NAME = "hackathon_wildlife_tracker_db"
 
-INPUT_FILE_PATH = os.getcwd() + "/input/images/"
+VIDEO_FILE_PATH = "input/videos"
 
+SIGHTING_FILE_PATH = os.getcwd() + "/input/images/"
 
 OUTPUT_FILE_PATH = os.getcwd() + "/output/my_output.json"
 
 
-def analyse_directory(directory=INPUT_FILE_PATH):
+def analyse_sightings(directory=SIGHTING_FILE_PATH):
     response_data = []
+
     for sighting in os.listdir(directory):
         sighting = os.path.join(directory, sighting)
         for filename in os.listdir(sighting):
             filename = os.path.join(sighting, filename)
+            metadata = None
+            if filename.endswith("metadata.json"):
+                with open(filename, "r") as file:
+                    metadata = json.load(file)
+
             with open(filename, "rb") as f:
                 BASE64_IMAGE_DATA = base64.standard_b64encode(f.read()).decode("utf-8")
                 image_type = "image/" + filename.split(".")[-1]
                 if image_type == "image/jpg":
                     image_type = "image/jpeg"
 
-            result = call_claude(image_data=BASE64_IMAGE_DATA, image_type=image_type)
+            if metadata is None:
+                raise ValueError("No metadata found for sighting")
+            
+            result = call_claude(image_data=BASE64_IMAGE_DATA, image_type=image_type, metadata=metadata)
 
             response_data.append(ast.literal_eval(result.text))
 
     return response_data
 
 
-def call_claude(image_data, image_type):
+def call_claude(image_data, image_type, metadata):
     client = anthropic.Anthropic()
 
     message = client.messages.create(
@@ -64,8 +77,8 @@ def call_claude(image_data, image_type):
     print(message.content)
     return message.content[0]
 
-
-response_data = analyse_directory()
+parser.process_videos(video_directory=VIDEO_FILE_PATH)
+response_data = analyse_sightings()
 
 common_name = []
 genus = []

013ee1826e4de5414c52086681f2253a35de1059
Minnie Taylor Manson <minnie.manson@kraken.tech>
2025-05-23 16:36:54 +0100

    Improve script to write output to json and clear table before running

--- script.py
+++ script.py
@@ -17,6 +17,30 @@ SIGHTING_FILE_PATH = os.getcwd() + "/input/images/"
 
 OUTPUT_FILE_PATH = os.getcwd() + "/output/my_output.json"
 
+conn = psycopg2.connect(
+    dbname=LOCAL_DB_NAME,
+    user="postgres",
+    password="hello",
+    host="localhost",
+    port="5432",
+)
+
+with conn:
+    with conn.cursor() as curs:
+        curs.execute(
+            """
+                DROP TABLE IF EXISTS wildlife;
+                CREATE TABLE IF NOT EXISTS wildlife (
+                id SERIAL PRIMARY KEY,
+                genus VARCHAR(255) NOT NULL,
+                species VARCHAR(255) NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                location VARCHAR(255),
+                time_spotted TIMESTAMP
+                );"""
+        )
+conn.commit()
+
 
 def analyse_sightings(directory=SIGHTING_FILE_PATH):
     response_data = []
@@ -31,6 +55,7 @@ def analyse_sightings(directory=SIGHTING_FILE_PATH):
                     metadata = json.load(file)
 
             with open(filename, "rb") as f:
+                print(f"\nProcessing {filename} \n")
                 BASE64_IMAGE_DATA = base64.standard_b64encode(f.read()).decode("utf-8")
                 image_type = "image/" + filename.split(".")[-1]
                 if image_type == "image/jpg":
@@ -68,7 +93,7 @@ def call_claude(image_data, image_type, metadata):
                     },
                     {
                         "type": "text",
-                        "text": "What animal is in this image? Please give the response as a dictionary in the following format: {'species': <species>, 'genus': <genus>, 'common_name': <name>}. Do not include any descriptions or explanations. Do not include any line breaks or new lines. Do not include any other text. Just give the dictionary.",
+                        "text": "What animal is in this image? Please give the response as a dictionary in the following format: {'species': <species>, 'genus': <genus>, 'common_name': <name>}. Do not include any descriptions or explanations. Do not include any line breaks or new lines. Do not include any other text. Just give the dictionary. These animals are United Kingdom species. For example, we do not have Racoons in the UK, we have Badgers instead. ",
                     },
                 ],
             }
@@ -90,15 +115,8 @@ for sighting in response_data:
 
 df = pd.DataFrame({"name": common_name, "species": species, "genus": genus})
 df.to_csv("output.csv", index=False)
+df.to_json("output.json", orient="records")
 
-
-conn = psycopg2.connect(
-    dbname=LOCAL_DB_NAME,
-    user="postgres",
-    password="hello",
-    host="localhost",
-    port="5432",
-)
 
 with conn:
     with conn.cursor() as curs:
